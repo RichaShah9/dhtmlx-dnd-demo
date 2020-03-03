7ee62f6526c97916ec0a1df8fced353e2ba367fb
Richa <r.shah@axelor.com>
2020-03-03 16:17:41 +0530

    Configure scheduler with app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Scheduler from "./components/Scheduler";
+import "./App.css";
 
 function App() {
+  
+  const onDragStart = e => {
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text", e.target.getAttribute("id"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-screen">
+      <div className="app-header">
+        <h1>Scheduler</h1>
+      </div>
+      <div className="app-page">
+        <div className="app-sidebar">
+          <h3>Drag Items Panel</h3>
+          <div
+            draggable="true"
+            onDragStart={onDragStart}
+            id="Event 1"
+            style={{
+              background: "lightblue",
+              padding: 15,
+              margin: 10
+            }}
+          >
+            Drag me into the list
+          </div>
+        </div>
+        <div className="app-content">
+          <Scheduler />
+        </div>
+      </div>
     </div>
   );
 }
